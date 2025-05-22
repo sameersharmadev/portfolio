d0e88a629b5129503dbb40af53f362652418edb6
sameersharmadev <sameerkw77@gmail.com>
2025-05-22 22:21:26 +0530

    updation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { NavLink } from 'react-router';
+import branding from '../assets/img/branding_header.png';
 
 const navLinks = [
   { to: "/", label: "Home" },
@@ -9,19 +10,27 @@ const navLinks = [
 
 export default function Header() {
   return (
-    <div className="flex bg-headerBeige text-black shadow-sm sticky top-0 z-50 text-center">
-      {navLinks.map(({ to, label }) => (
-        <NavLink
-          key={to}
-          to={to}
-          className={({ isActive, isPending }) =>
-            `relative p-2 px-8 transition-all duration-200 ease-in-out
-            ${isActive ? 'bg-black text-headerBeige' : 'hover:bg-black hover:text-headerBeige'}`
-          }
-        >
-          {label}
-        </NavLink>
-      ))}
+    <div className="flex justify-between items-center bg-headerBeige text-black shadow-sm sticky top-0 z-50">
+      {/* Navigation Links */}
+      <div className="flex">
+        {navLinks.map(({ to, label }) => (
+          <NavLink
+            key={to}
+            to={to}
+            className={({ isActive }) =>
+              `p-2 px-6 transition-all duration-200 ease-in-out 
+              ${isActive ? 'bg-black text-headerBeige' : 'hover:bg-black hover:text-headerBeige'}`
+            }
+          >
+            {label}
+          </NavLink>
+        ))}
+      </div>
+
+      {/* Branding */}
+      <div className="text-lg tracking-wide font-bold tracking-wider text-black uppercase select-none cursor-default px-8">
+        {"< Sameer Sharma />"}
+      </div>
     </div>
   );
 }
